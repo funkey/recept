608c52cedb67523a31cefa12ce04cde4924695a4
Jan Funke <funkej@janelia.hhmi.org>
2020-11-11 09:12:51 -0500

    Add install script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "This script will install a library that will intercept pen events and smooth them."
+echo
+echo "If you haven't set up public key authentication on your remarkable yet, now would"
+echo "be a good time to do so (otherwise you'll have to type your password multiple"
+echo "times)."
+echo
+echo "Either way, make sure you have your remarkable password written down somewhere, you"
+echo "might otherwise risk to lock yourself out if the GUI does not start up anymore."
+echo
+
+read -p "Enter the hostname or IP address of your remarkable device [remarkable]:" remarkable
+remarkable=${remarkable:-remarkable}
+
+scp ./libintercept.so root@$remarkable:/usr/lib/
+ssh $remarkable "grep -qxF 'Environment=LD_PRELOAD=/usr/lib/libintercept.so' /lib/systemd/system/xochitl.service || sed -i 's#\[Service\]#[Service]\nEnvironment=LD_PRELOAD=/usr/lib/libintercept.so#' /lib/systemd/system/xochitl.service"
+ssh $remarkable "systemctl daemon-reload; systemctl restart xochitl"
